32f1e0970287a85c75dc8479df9a423c0ac12104
dogdduddy <dogdduddy@gmail.com>
2023-01-24 23:48:26 +0900

    feat : 파이어베이스 연결 및 Facebook 연결
    이미 생성 된 Ingredient Firebase 프로젝트에 추가 연결
    Facebook Developer에 새로운 앱 등록 및 테스트 버전으로 연결

---
+++ app/src/main/java/com/dogdduddy/ingredient/App.kt
@@ -0,0 +1,34 @@
+package com.dogdduddy.ingredient
+
+import android.app.Application
+import android.content.Context
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+
+class App: Application() {
+    companion object {
+        var instance: App? = null
+        var appContext : Context? = null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+        instance = this
+
+        FacebookSdk.sdkInitialize(getApplicationContext())
+        AppEventsLogger.activateApp(this)
+
+    }
+    override fun onTerminate() {
+        super.onTerminate()
+        instance = null
+    }
+
+    fun getAppContext() : App {
+        checkNotNull(instance){
+            "This Application does not inherit com.example.App"
+        }
+        return instance!!
+    }
+}
